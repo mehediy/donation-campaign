e8c822626b833dfb6ff4fcb225c37f61a8b3a64c
Mehedi Hasan <113818748+mehediy@users.noreply.github.com>
2023-09-26 13:48:19 +0600

    added pie chart

--- src/Pages/Statistics.jsx
+++ src/Pages/Statistics.jsx
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Chart from "react-google-charts";
+import { useLoaderData } from "react-router-dom";
 
 const Statistics = () => {
-  return <div>Statistics</div>;
+  const { donation_data } = useLoaderData();
+  const [donatedData, setDonatedData] = useState([]);
+  useEffect(() => {
+    const localData = JSON.parse(localStorage.getItem("donated"));
+    if (localData) setDonatedData(localData);
+  }, []);
+
+  const data = [
+    ["Task", "Donation"],
+    ["Your Donation", donatedData.length],
+    ["Total Donation", donation_data.length],
+  ];
+
+  return (
+    <div className="container mx-auto">
+      <Chart
+        chartType="PieChart"
+        data={data}
+        width={"100%"}
+        height={"400px"}
+        options={{
+          legend: {
+            position: "bottom",
+          },
+        }}
+      />
+    </div>
+  );
 };
 
 export default Statistics;
--- src/Router/router.jsx
+++ src/Router/router.jsx
@@ -22,6 +22,7 @@ const router = createBrowserRouter([
       {
         path: "/statistics",
         element: <Statistics />,
+        loader: () => fetch("https://mehediy.github.io/data.json"),
       },
       {
         path: "/donation_details/:id",
